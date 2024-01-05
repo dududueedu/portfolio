0eebdeb73beaa99af608a505b2aa289f95190c04
Eduardo Gomes <eduardooffside@gmail.com>
2024-01-05 17:10:43 -0300

    up: home - add: link

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -18,4 +18,8 @@ export class HomeComponent implements OnInit {
   Se você tem um ótimo projeto que precisa de habilidades incríveis e muita organização nos processos,
   eu sou a pessoa certa.`
 
+  link_instagram = "https://www.instagram.com/e.duus?igsh=YjJ0NGttaWZicGJj"
+  link_github = "https://github.com/dududueedu"
+  link_facebook = "https://www.facebook.com/profile.php?id=100005503066960&mibextid=ZbWKwL"
+  link_linkedin = "https://www.linkedin.com/in/dududueedu"
 }
